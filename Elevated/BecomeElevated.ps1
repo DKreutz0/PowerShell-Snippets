b175cb74cfe3d6ac2abb8b8c2a75c944295ce237
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-08 18:36:41 +0100

    /

--- Elevated/BecomeElevated.ps1
+++ Elevated/BecomeElevated.ps1
@@ -1,11 +1,14 @@
+param([switch]$Elevated)
+$Elevated = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 if (!((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)))  {
-    Write-Host "Cannot become a privileged user. The script is aborting" -BackgroundColor red -ForegroundColor white
-    Start-Sleep 5
-    Exit
-} 
-else {
-    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    if (!($Elevated)) {
+        Write-Host "Cannot become a privileged user. The script is aborting" -BackgroundColor red -ForegroundColor white
+        Start-Sleep 5
+        Exit
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
 }
-
 
 #place code here.

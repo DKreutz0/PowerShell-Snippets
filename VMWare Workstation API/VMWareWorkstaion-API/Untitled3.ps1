c30fef6c75363b34fa9a915d5043e73725aadba6
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-12 21:41:09 +0100

    cleanup

--- VMWare Workstation API/VMWareWorkstaion-API/Untitled3.ps1
+++
@@ -1,3 +0,0 @@
-﻿$Apps = @()
-$Apps += Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | where { $_.displayname -like '*vmware*'} # 32 Bit
-$Apps += Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | where { $_.displayname -like '*vmware*'}            # 64 Bit

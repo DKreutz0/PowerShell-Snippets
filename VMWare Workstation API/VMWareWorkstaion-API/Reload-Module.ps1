c06eeb5d4250f1e222b976b3a07e488385861ee3
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-22 19:35:19 +0100

    .

--- VMWare Workstation API/VMWareWorkstaion-API/Reload-Module.ps1
+++ VMWare Workstation API/VMWareWorkstaion-API/Reload-Module.ps1
@@ -1,7 +1,7 @@
 ﻿rv -name * -ErrorAction SilentlyContinue
 rv VMwareWorkstationConfigParameters -ErrorAction SilentlyContinue
 Remove-Module VMWareWorkstaion-API -Force -ErrorAction SilentlyContinue 
-Import-Module VMWareWorkstaion-API -Force -ErrorAction SilentlyContinue
+Import-Module VMWareWorkstaion-API -Force -ErrorAction SilentlyContinue 
 Test-ModuleManifest -Path "$PSScriptRoot\VMWareWorkstaion-API.psd1"
 
 

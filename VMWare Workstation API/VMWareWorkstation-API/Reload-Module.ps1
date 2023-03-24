cc984cf637d3091ec5da0c922fde38b4cd205493
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-24 23:08:08 +0100

    new

---
+++ VMWare Workstation API/VMWareWorkstation-API/Reload-Module.ps1
@@ -0,0 +1,15 @@
+﻿rv -name * -ErrorAction SilentlyContinue
+rv VMwareWorkstationConfigParameters -ErrorAction SilentlyContinue
+Remove-Module VMWareWorkstation-API -Force -ErrorAction SilentlyContinue 
+Import-Module VMWareWorkstation-API -Force -ErrorAction SilentlyContinue 
+Test-ModuleManifest -Path "$PSScriptRoot\VMWareWorkstation-API.psd1"
+
+
+<#
+vm.cfg inlezen en vmrest.exe afmaken
+
+$EnumSpecialFolders = [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) | Sort-Object 
+$GetSpecialFolder = [Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)
+$GetUserProfile = [Environment]::GetFolderPath([Environment+SpecialFolder]::UserProfile)
+#>
+

2ca0b64a80d1a0e4c292a77728305f5d0f912c7e
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-25 00:11:10 +0100

    .

---
+++ VMWare Workstation API/old/# Gather Folder information/Untitled5.ps1
@@ -0,0 +1,110 @@
+﻿ $Global:VMwareWorkstationConfigParameters = New-Object PSObject
+    try {
+
+        if ([string]::IsNullOrEmpty($VMwareWorkstationConfigParameters.InstallLocation)) {
+            
+            Write-Message -Message "Could not find the installation path in the registry. Please select the Vmware Workstation installation path" -MessageType INFORMATION
+
+            [void]::([int]$RetryRetrieveFolder = 0)
+            [bool]$RetryRetrieveFolderError = $false 
+            
+            do {
+                $FolderBrowserDialogPath = FindFiles -Parameter GetVMWareWorkstationInstallationPath
+
+                switch ($FolderBrowserDialogPath.GetType().Name) {
+                    String {
+                       if ($FolderBrowserDialogPath -eq "CANCEL") {
+                            break
+                       }
+                       if ($FolderBrowserDialogPath -eq "EMPTY") {
+
+                            $RetryRetrieveFolderError = $true
+                            
+                            switch ($RetryRetrieveFolder) {
+                                0 { Write-Message -Message "The Path provide did not contain the vmware installation, please retry" -MessageType INFORMATION }
+                                1 { Write-Message -Message "The Path provide did not contain the vmware installation, please retry, after this attempt the script will stop." -MessageType INFORMATION }
+                                2 { Write-Error -Exception "Path Not found" -ErrorAction Stop}
+                            }
+                        }
+                    }
+                    FileInfo {
+
+                        $RetryRetrieveFolderError = $false               
+
+                        $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $(Join-Path $FolderBrowserDialogPath.Directory -ChildPath "\") -Force -ErrorAction Stop
+
+                        if ($VmwareCouldNotDetermine_Win32_Products) {
+                            $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "Name" -Value "VMware Workstation" -Force -ErrorAction Stop
+                        }
+
+                        $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "Version" -Value "$([System.Diagnostics.FileVersionInfo]::GetVersionInfo($FolderBrowserDialogPath.FullName) | Select-Object -ExpandProperty FileVersion)" -Force
+                        Write-Message -Message "Vmware Workstation $($VMwareWorkstationConfigParameters.Version) Installlocation defined as: $($VMwareWorkstationConfigParameters.InstallLocation)" -MessageType INFORMATION
+                        break
+                    }
+                }
+                [void]::($RetryRetrieveFolder++)
+
+            } until (($RetryRetrieveFolder -gt 3) -or (!([string]::IsNullOrEmpty($VMwareWorkstationConfigParameters.InstallLocation))))
+
+        }
+    }
+
+    catch {
+         try {
+             if ($RetryRetrieveFolderError) {
+                Write-Message -Message "Doing a alternative scan - Scanning all filesystem disks that are found on your system" -MessageType INFORMATION
+                $CollectDriveLetters = $(Get-PSDrive -PSProvider FileSystem ) | Select-Object -ExpandProperty Root
+                $Collected = [System.Collections.ArrayList]@()
+                $CollectDriveLetters | ForEach-Object { $Collected += Get-ChildItem -Path $($_) -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq "vmware.exe" } }
+                [void]::($RetryRetrieveFolder = 0)
+                if (!([string]::IsNullOrEmpty($Collected))) {
+                    if ($Collected.count -le 1) {
+                           $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "Name" -Value "VMware Workstation" -Force -ErrorAction Stop
+                           $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "Version" -Value "$([System.Diagnostics.FileVersionInfo]::GetVersionInfo($Collected.fullname) | Select-Object -ExpandProperty FileVersion)" -Force
+                           $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $Collected.DirectoryName -Force -ErrorAction Stop
+                           $VmwareCouldNotDetermine_Win32_Products = $true
+                    }
+
+                    if ($Collected.count -gt 1) {
+                        do {
+                            $SelectedPath = $Collected | Select-Object Name,fullname,DirectoryName | Out-GridView -Title "Multiple VMWare Workstation installation folders found, please select the folder where VMWare Workstation is installed" -OutputMode Single
+                            if ($null -ne $SelectedPath) {
+                                if (Test-Path $SelectedPath.FullName -ErrorAction Stop) {
+                                    $Global:VMwareWorkstationConfigParameters | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $SelectedPath.DirectoryName -Force -ErrorAction Stop
+                                    $RetryRetrieveFolderError = $false
+                                    break
+                                }
+                            }
+                            else {                        
+                                if ($RetryRetrieveFolder -lt 1) {
+                                    Write-Message -Message "No input gathered, retrying" -MessageType INFORMATION
+                                    $VmwareCouldNotDetermine_Win32_Products = $false
+                                }
+                                if ($RetryRetrieveFolder -gt 1) {
+                                    Write-Message -Message "No input gathered, last retry" -MessageType INFORMATION
+                                    Write-Error -Exception "Path Not found" -ErrorAction Stop
+                                    break
+                                }
+                                [void]::($RetryRetrieveFolder++)
+                            }
+                        
+                        } until ($RetryRetrieveFolder -ge 2)
+                    }
+                }
+             }
+             else {
+                  Write-Error -Exception "Path Not found" -ErrorAction Stop
+             }
+        }
+        catch {
+                 Write-Message -Message "Unknown error occured the script is quitting" -MessageType ERROR            
+        }
+
+        if ([string]::IsNullOrEmpty($VMwareWorkstationConfigParameters.InstallLocation)) {
+            Write-Message -Message "Cannot determine if VMWare Workstation is installed on this machine, the script is quitting" -MessageType ERROR
+            $RetryRetrieveFolder = $false
+        }
+    }
+    finally {
+     Remove-Variable -Name VmwareCouldNotDetermine_Win32_Products -ErrorAction SilentlyContinue
+    }

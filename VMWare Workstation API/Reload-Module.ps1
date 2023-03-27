b0ead8df8d85d4293c1fa97fce5bfb7e2de0fb3b
Dennis Kreutz <d.kreutz@dictu.nl>
2023-03-27 23:54:27 +0200

    aanpassing

--- VMWare Workstation API/Reload-Module.ps1
+++ VMWare Workstation API/Reload-Module.ps1
@@ -1,5 +1,5 @@
-﻿rv -name * -ErrorAction SilentlyContinue
-rv VMwareWorkstationConfigParameters -ErrorAction SilentlyContinue
+﻿Remove-Variable -name * -ErrorAction SilentlyContinue
+Remove-Variable VMwareWorkstationConfigParameters -ErrorAction SilentlyContinue
 Remove-Module VMWareWorkstation-API -Force -ErrorAction SilentlyContinue 
 Import-Module VMWareWorkstation-API -Force -ErrorAction SilentlyContinue 
 Test-ModuleManifest -Path "$PSScriptRoot\VMWareWorkstation-API.psd1"
@@ -7,10 +7,7 @@ Test-ModuleManifest -Path "$PSScriptRoot\VMWareWorkstation-API.psd1"
 
 <#
 vm.cfg inlezen en vmrest.exe afmaken
-
 $EnumSpecialFolders = [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) | Sort-Object 
 $GetSpecialFolder = [Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)
 $GetUserProfile = [Environment]::GetFolderPath([Environment+SpecialFolder]::UserProfile)
 #>
-
-	
